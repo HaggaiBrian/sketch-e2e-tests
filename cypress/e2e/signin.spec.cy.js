1241289a88e38706a54f9b267538071b20bb0e02
haggaibrian <haggzo@gmail.com>
2022-06-11 00:06:38 +0300

    Added one more positive case and one negative case

--- cypress/e2e/signin.spec.cy.js
+++ cypress/e2e/signin.spec.cy.js
@@ -2,12 +2,30 @@ import { Signin } from "../page_objects/Signin";
 
 const signin = new Signin()
 
+beforeEach(() => 
+{
+    signin.navigate()
+})
+
 describe('Positive test case scenarios', () => 
 {
     it('should be able to sign-in with correct email and password', () =>
     {
-        signin.navigate()
-        signin.login('haggzo@gmail.com','Pentagonhouse1@')
+        signin.loginFilled('haggzo@gmail.com','Pentagonhouse1@')
         signin.validateSuccessfulSignin()
+    })
+
+    it('should be able to sign-out when signed in', () => 
+    {
+        signin.loginFilled('haggzo@gmail.com','Pentagonhouse1@')
+        signin.signout()
+    })  
+})
+
+describe('Negative test case scenarios', () => 
+{
+    it('should not be able to sign-in with both email and password blank', () =>
+    {
+        signin.loginBlank()
     })
 })
